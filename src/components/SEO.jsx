c32a5524a9501164ecb3d904910be9f3cdf14d1b
Auqab <zeusata34@gmail.com>
2026-08-05 18:01:42 +0300

    Add Google Search Console verification

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -14,21 +14,22 @@ return(
 
 
 <meta
-
 name="description"
-
 content={description}
-
 />
 
 
 <meta
-
 name="viewport"
-
 content="width=device-width, initial-scale=1.0"
-
 />
+
+
+<meta
+name="google-site-verification"
+content="uB2mNeZn8rdB0CAauiQ10SSqV9Hbc3KOlRWeAn57RQc"
+/>
+
 
 </Helmet>
 
